cb5e1469bfabc346dd89391c86b0cac61ea12524
sanjaysanjay182410-creator <sanjaysanjay182410@gmail.com>
2026-07-09 10:06:40 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 10:06:40 AM
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        int first = 1;
+        int second = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+
+        return second;
+    }
+}
