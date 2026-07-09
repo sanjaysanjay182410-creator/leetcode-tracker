7429590936fac931b3f0a444c5ac719c8a30a7b2
sanjaysanjay182410-creator <sanjaysanjay182410@gmail.com>
2026-07-09 10:07:12 +0530

    Create 24-SwapNodesInPairs

---
+++ 24-SwapNodesInPairs/24-SwapNodesInPairs.java
@@ -0,0 +1,26 @@
+// Last updated: 7/9/2026, 10:07:12 AM
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        while (prev.next != null && prev.next.next != null) {
+
+            ListNode first = prev.next;
+            ListNode second = first.next;
+
+            // Swap
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            // Move to the next pair
+            prev = first;
+        }
+
+        return dummy.next;
+    }
+}
