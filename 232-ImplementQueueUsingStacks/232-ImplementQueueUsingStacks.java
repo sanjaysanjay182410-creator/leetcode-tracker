5ea5001d8a64eb136cd1d803619f7a0bf689c7c1
sanjaysanjay182410-creator <sanjaysanjay182410@gmail.com>
2026-09-23 14:47:26 +0530

    Create 232-ImplementQueueUsingStacks

---
+++ 232-ImplementQueueUsingStacks/232-ImplementQueueUsingStacks.java
@@ -0,0 +1,36 @@
+// Last updated: 9/23/2026, 2:47:24 PM
+class MyQueue {
+    private Stack<Integer> inStack;
+    private Stack<Integer> outStack;
+    
+    public MyQueue() {
+        inStack = new Stack<>();
+        outStack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        inStack.push(x);
+    }
+    
+    public int pop() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.pop();
+    }
+    
+    public int peek() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.peek();
+    }
+    
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+}
