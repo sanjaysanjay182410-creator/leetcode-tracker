7ee5a3f84499a24adf242b64f8a5f6bf4fdf5cd1
sanjaysanjay182410-creator <sanjaysanjay182410@gmail.com>
2026-09-23 14:47:41 +0530

    Create 229-MajorityElementIi

---
+++ 229-MajorityElementIi/229-MajorityElementIi.java
@@ -0,0 +1,62 @@
+// Last updated: 9/23/2026, 2:47:39 PM
+import java.util.*;
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+
+        List<Integer> result = new ArrayList<>();
+
+        int candidate1 = 0;
+        int candidate2 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        // Find two possible candidates
+        for (int num : nums) {
+
+            if (num == candidate1) {
+                count1++;
+            } 
+            else if (num == candidate2) {
+                count2++;
+            } 
+            else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } 
+            else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } 
+            else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Verify candidates
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            }
+            if (num == candidate2) {
+                count2++;
+            }
+        }
+
+        int n = nums.length;
+
+        if (count1 > n / 3) {
+            result.add(candidate1);
+        }
+
+        if (candidate2 != candidate1 && count2 > n / 3) {
+            result.add(candidate2);
+        }
+
+        return result;
+    }
+}
