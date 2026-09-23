0005f2dce0a08c9de137dac69c5b2cc4d1ed15a8
sanjaysanjay182410-creator <sanjaysanjay182410@gmail.com>
2026-09-23 14:57:24 +0530

    Create 44-WildcardMatching

---
+++ 44-WildcardMatching/44-WildcardMatching.java
@@ -0,0 +1,31 @@
+// Last updated: 9/23/2026, 2:57:22 PM
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int i = 0, j = 0;
+        int star = -1, match = 0;
+
+        while (i < s.length()) {
+            if (j < p.length() &&
+                (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))) {
+                i++;
+                j++;
+            } else if (j < p.length() && p.charAt(j) == '*') {
+                star = j;
+                match = i;
+                j++;
+            } else if (star != -1) {
+                j = star + 1;
+                match++;
+                i = match;
+            } else {
+                return false;
+            }
+        }
+
+        while (j < p.length() && p.charAt(j) == '*') {
+            j++;
+        }
+
+        return j == p.length();
+    }
+}
