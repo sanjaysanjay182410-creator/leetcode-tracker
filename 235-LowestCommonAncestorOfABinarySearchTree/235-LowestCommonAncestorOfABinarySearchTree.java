e661a00f9a8047ff644a7fe872ec9b6c7f52f59d
sanjaysanjay182410-creator <sanjaysanjay182410@gmail.com>
2026-09-23 14:47:13 +0530

    Create 235-LowestCommonAncestorOfABinarySearchTree

---
+++ 235-LowestCommonAncestorOfABinarySearchTree/235-LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,24 @@
+// Last updated: 9/23/2026, 2:47:11 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;          // both in left subtree
+            } else if (p.val > root.val && q.val > root.val) {
+                root = root.right;         // both in right subtree
+            } else {
+                return root;               // current node is the LCA
+            }
+        }
+        return null;
+    }
+}
