21d41cc31c54f0ddafe3d3aea97e24457962ad32
sanjaysanjay182410-creator <sanjaysanjay182410@gmail.com>
2026-09-23 14:57:49 +0530

    Create 36-ValidSudoku

---
+++ 36-ValidSudoku/36-ValidSudoku.java
@@ -0,0 +1,24 @@
+// Last updated: 9/23/2026, 2:57:47 PM
+import java.util.HashSet;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+
+                if (num != '.') {
+                    if (!seen.add(num + " in row " + i) ||
+                        !seen.add(num + " in col " + j) ||
+                        !seen.add(num + " in box " + (i / 3) + "-" + (j / 3))) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
