e7e1d2830da6c70abcdc0753161567598ab16657
sanjaysanjay182410-creator <sanjaysanjay182410@gmail.com>
2026-09-23 14:57:11 +0530

    Create 47-PermutationsIi

---
+++ 47-PermutationsIi/47-PermutationsIi.java
@@ -0,0 +1,42 @@
+// Last updated: 9/23/2026, 2:57:10 PM
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        boolean[] visited = new boolean[nums.length];
+        backtrack(nums, visited, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int[] nums, boolean[] visited,
+                           List<Integer> current,
+                           List<List<Integer>> result) {
+
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (visited[i]) {
+                continue;
+            }
+
+            // Skip duplicate numbers
+            if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) {
+                continue;
+            }
+
+            visited[i] = true;
+            current.add(nums[i]);
+
+            backtrack(nums, visited, current, result);
+
+            current.remove(current.size() - 1);
+            visited[i] = false;
+        }
+    }
+}
