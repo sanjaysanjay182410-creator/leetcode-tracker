7b921ea9a571b7d8ca2effae96cf9934b6e4776f
sanjaysanjay182410-creator <sanjaysanjay182410@gmail.com>
2026-09-23 14:49:45 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.java
@@ -0,0 +1,26 @@
+// Last updated: 9/23/2026, 2:49:43 PM
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = sumOfSquares(slow);
+            fast = sumOfSquares(sumOfSquares(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+    private int sumOfSquares(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
