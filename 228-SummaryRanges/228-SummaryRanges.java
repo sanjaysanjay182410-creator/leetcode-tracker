64c99a1b3d9153dd781563bdf6c3d33da52b8f6c
sanjaysanjay182410-creator <sanjaysanjay182410@gmail.com>
2026-09-23 14:47:48 +0530

    Create 228-SummaryRanges

---
+++ 228-SummaryRanges/228-SummaryRanges.java
@@ -0,0 +1,34 @@
+// Last updated: 9/23/2026, 2:47:46 PM
+import java.util.*;
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+
+        List<String> result = new ArrayList<>();
+
+        int i = 0;
+
+        while (i < nums.length) {
+
+            int start = nums[i];
+
+            // Find the end of consecutive sequence
+            while (i + 1 < nums.length &&
+                   (long) nums[i + 1] == (long) nums[i] + 1) {
+                i++;
+            }
+
+            int end = nums[i];
+
+            if (start == end) {
+                result.add(String.valueOf(start));
+            } else {
+                result.add(start + "->" + end);
+            }
+
+            i++;
+        }
+
+        return result;
+    }
+}
