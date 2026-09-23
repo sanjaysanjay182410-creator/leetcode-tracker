bdabddcbb4e2f00c2510e1cb0fcbacce4c62d936
sanjaysanjay182410-creator <sanjaysanjay182410@gmail.com>
2026-09-23 14:48:56 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -0,0 +1,18 @@
+// Last updated: 9/23/2026, 2:48:54 PM
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+
+            set.add(num);
+        }
+
+        return false;
+    }
+}
