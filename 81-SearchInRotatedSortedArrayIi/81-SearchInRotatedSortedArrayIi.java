7e6494ce024b08745e868cde3ed56daca80b6a84
sanjaysanjay182410-creator <sanjaysanjay182410@gmail.com>
2026-09-23 14:55:22 +0530

    Create 81-SearchInRotatedSortedArrayIi

---
+++ 81-SearchInRotatedSortedArrayIi/81-SearchInRotatedSortedArrayIi.java
@@ -0,0 +1,41 @@
+// Last updated: 9/23/2026, 2:55:20 PM
+class Solution {
+    public boolean search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            // Skip duplicates
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            }
+            // Left half is sorted
+            else if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
